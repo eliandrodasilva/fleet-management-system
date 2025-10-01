f334a6731549a2490f76761e55bb553eea53550b
Eliandro <eliandro.gomes42@gmail.com>
2025-10-01 16:33:38 -0300

    feat(jpa): add VehicleDAO with findByLicensePlate and findByStatus methods

---
+++ src/main/java/dao/VehicleDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import model.Vehicle;
+
+import java.util.List;
+
+public class VehicleDAO extends GenericDAO<Vehicle, Long> {
+
+    public VehicleDAO() {
+        super(Vehicle.class);
+    }
+
+    public Vehicle findByLicensePlate(String licensePlate) {
+        List<Vehicle> list = em.createQuery(
+                        "FROM Vehicle v WHERE v.licensePlate = :plate", Vehicle.class)
+                .setParameter("plate", licensePlate)
+                .getResultList();
+        return list.isEmpty() ? null : list.get(0);
+    }
+
+    public List<Vehicle> findByStatus(String status) {
+        return em.createQuery(
+                        "FROM Vehicle v WHERE v.status = :status", Vehicle.class)
+                .setParameter("status", status)
+                .getResultList();
+    }
+}
