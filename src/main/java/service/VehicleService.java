e0d1f14faab1c09a96fa547e7943d0d5898ab8a8
Eliandro <eliandro.gomes42@gmail.com>
2025-10-01 20:54:59 -0300

    refactor(jpa): add findAllVehicles() method to VehicleService

--- src/main/java/service/VehicleService.java
+++ src/main/java/service/VehicleService.java
@@ -48,6 +48,8 @@ public class VehicleService {
         }
     }
 
+    public List<Vehicle> getAllVehicles() {return vehicleDAO.findAll();}
+
     public Vehicle getVehicleById(Long id) {
         return vehicleDAO.findById(id);
     }
