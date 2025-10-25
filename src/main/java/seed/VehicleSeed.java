5dd38c84e75cd1cdfcbfa2dc7296cc809c4aed3e
Eliandro <eliandro.gomes42@gmail.com>
2025-10-24 22:13:53 -0300

    fix(seed): fixed to not use enums anymore

--- src/main/java/seed/VehicleSeed.java
+++ src/main/java/seed/VehicleSeed.java
@@ -1,25 +1,23 @@
 package seed;
 
 import model.Vehicle;
-import model.enums.VehicleModel;
+import model.VehicleModel;
 import model.enums.VehicleStatus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class VehicleSeed {
-
-    public static List<Vehicle> getVehicles() {
+    public static List<Vehicle> getVehicles(List<VehicleModel> models) {
         List<Vehicle> vehicles = new ArrayList<>();
 
-        vehicles.add(new Vehicle("ABC1234", VehicleModel.PARADISO_1200, 2015, 120000, VehicleStatus.AVAILABLE));
-        vehicles.add(new Vehicle("JKL2468", VehicleModel.PARADISO_1800_DD, 2021, 60000, VehicleStatus.AVAILABLE));
-        vehicles.add(new Vehicle("XYZ5678", VehicleModel.MERCEDES_O500, 2018, 80000, VehicleStatus.IN_MAINTENANCE));
-        vehicles.add(new Vehicle("DEF9876", VehicleModel.VOLVO_9700, 2020, 50000, VehicleStatus.IN_USE));
-        vehicles.add(new Vehicle("MNO1357", VehicleModel.VOLVO_9900, 2022, 40000, VehicleStatus.IN_USE));
-        vehicles.add(new Vehicle("GHI5432", VehicleModel.LUMINA_COMIL, 2019, 75000, VehicleStatus.INACTIVE));
-        vehicles.add(new Vehicle("HOT6969", VehicleModel.VOYAGE, 1987, 120000,VehicleStatus.AVAILABLE));
-
+        vehicles.add(new Vehicle("ABC1234", models.get(0), 2015, 120000, VehicleStatus.AVAILABLE));
+        vehicles.add(new Vehicle("JKL2468", models.get(1), 2021, 60000, VehicleStatus.AVAILABLE));
+        vehicles.add(new Vehicle("XYZ5678", models.get(2), 2018, 80000, VehicleStatus.IN_MAINTENANCE));
+        vehicles.add(new Vehicle("DEF9876", models.get(3), 2020, 50000, VehicleStatus.IN_USE));
+        vehicles.add(new Vehicle("MNO1357", models.get(4), 2022, 40000, VehicleStatus.IN_USE));
+        vehicles.add(new Vehicle("GHI5432", models.get(5), 2019, 75000, VehicleStatus.INACTIVE));
+        vehicles.add(new Vehicle("HOT6969", models.get(6), 1987, 120000, VehicleStatus.AVAILABLE));
         return vehicles;
     }
 }
