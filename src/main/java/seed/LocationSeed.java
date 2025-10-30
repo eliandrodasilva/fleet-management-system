7267a9c4491af8ea747e3994bf095c5aa713e498
Eliandro <eliandro.gomes42@gmail.com>
2025-10-30 01:51:51 -0300

    chore(seed): add more locations to database

--- src/main/java/seed/LocationSeed.java
+++ src/main/java/seed/LocationSeed.java
@@ -10,11 +10,19 @@ public class LocationSeed {
     public static List<Location> getLocations() {
         List<Location> locations = new ArrayList<>();
 
-        locations.add(new Location("Garagem Central"));
-        locations.add(new Location("Cidade Gaúcha (Garagem Central)"));
-        locations.add(new Location("Tamboara (Garagem Secundária)"));
-        locations.add(new Location("Oficina Norte"));
+        locations.add(new Location("Localidade de Teste"));
         locations.add(new Location("Terminal Rodoviário Guaíra (Paraguai)"));
+        locations.add(new Location("Umuarama"));
+        locations.add(new Location("Cidade Gaúcha"));
+        locations.add(new Location("Cianorte"));
+        locations.add(new Location("Paraíso do Norte"));
+        locations.add(new Location("Tamboara"));
+        locations.add(new Location("Paranavaí"));
+        locations.add(new Location("Maringá"));
+        locations.add(new Location("Arapongas"));
+        locations.add(new Location("Londrina"));
+        locations.add(new Location("Oficina (Umuarama)"));
+        locations.add(new Location("Oficina (Londrina)"));
 
         return locations;
     }
