cc84dc120354f1daeb0c2317e6d98b7fdbeb9f05
Eliandro <eliandro.gomes42@gmail.com>
2025-11-23 20:39:35 -0300

    feat(dao): add existsByDriver and existsByVehicle queries to RouteDAO

--- src/main/java/dao/RouteDAO.java
+++ src/main/java/dao/RouteDAO.java
@@ -6,5 +6,19 @@ public class RouteDAO extends AbstractDAOImpl<Route, Long>{
 
     public RouteDAO() {}
 
+    public boolean existsByDriver(Long driverId) {
+        return em.createQuery(
+                        "SELECT COUNT(r) FROM Route r WHERE r.driver.id = :driverId", Long.class
+                )
+                .setParameter("driverId", driverId)
+                .getSingleResult() > 0;
+    }
 
+    public boolean existsByVehicle(Long vehicleId) {
+        return em.createQuery(
+                        "SELECT COUNT(r) FROM Route r WHERE r.vehicle.id = :vehicleId", Long.class
+                )
+                .setParameter("vehicleId", vehicleId)
+                .getSingleResult() > 0;
+    }
 }
